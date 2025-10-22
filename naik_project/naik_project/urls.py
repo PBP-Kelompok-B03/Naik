b0b222b52052aa3cea8017e96b0bd85c2e17b4e8
Hammam11 <hammam.muhammad@ui.ac.id>
2025-10-22 23:30:58 +0700

    Move checkout app to project root

--- naik_project/naik_project/urls.py
+++ naik_project/naik_project/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('checkout/', include('checkout.urls')),
+    path('comments/', include('comments.urls')),
+    path('main/', include('main.urls')),
+    path('auction/', include('auction.urls')),
 ]
